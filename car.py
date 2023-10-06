fd180b2ef063bf3df5b2f8a03e3533db2c6a3864
MissTipo <105214329+MissTipo@users.noreply.github.com>
2023-10-06 16:01:45 +0300

    Update add tire attribute to Car class

--- car.py
+++ car.py
@@ -5,10 +5,11 @@ from serviceable import Serviceable
 class Car(Serviceable):
     """Car class for the car service program."""
 
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tires):
         """Initialise a Car object."""
         self.engine = engine
         self.battery = battery
+        self.tires = tires
 
     def needs_service(self) -> bool:
         """Return True if the car needs a service."""
